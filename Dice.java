fc41a1b315338b9a02d11d85d4196da68983265f
Sumaiya-Yesmin <sumaiya.yesmin.cse@gmail.com>
2025-07-03 00:35:42 +0900

    Update Dice.java
    Won/lost message added

--- Dice.java
+++ Dice.java
@@ -6,16 +6,20 @@ public class Dice {
         Random rn = new Random();
         int dice1 = rn.nextInt(6);
         System.out.println("Rolling the dice...");
-        System.out.println("Die 1: " + dice1);
+        System.out.println("Dice 1: " + dice1);
         Random rn1 = new Random();
         int dice2 = rn1.nextInt(6);
-        System.out.println("Die 2: " + dice2);
+        System.out.println("Dice 2: " + dice2);
         int total = dice1+dice2;
         System.out.println("Total value:"+ total);
+        if (total > 7) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
 
     }
 }
-
 
 
 
